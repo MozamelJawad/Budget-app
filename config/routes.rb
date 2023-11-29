605e89cc86abda568d979a16172487a9ec70829a
Mozamel Jawad <mozameljawad@gmail.com>
2023-11-29 22:45:34 +0430

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
 
- root to: "home#index"
+  unauthenticated :user do
+    root 'home#index', as: :unauthenticated_root
+  end
+
+
+  resources :categories
+  resources :expenses
 
 end
